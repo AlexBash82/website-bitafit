a3b523686b6b7d61d8722011d4de454ebbaa8234
AlexBash82 <kometa.4m@gmail.com>
2022-09-15 18:32:21 +0500

    add retina image

--- src/Components/Promo/Promo.js
+++ src/Components/Promo/Promo.js
@@ -62,17 +62,17 @@ function Promo({ elementToScrollRef }) {
                 Скачай приложение
               </div>
               <div className="Promo_box_download_item_col-1_row-2">
-                {(large || isIphone) && (
-                  <a href="https://apps.apple.com/ru/app/bitafit/id1591904801?l=en">
-                    <div className="Promo_box_download_item_col-1_row-2_button-2">
-                      <div className="Promo_box_download_item_col-1_row-2_button-2_icon-2"></div>
+                {(large || !isIphone) && (
+                  <a href="https://play.google.com/store/apps/details?id=com.bitafit.client">
+                    <div className="Promo_box_download_item_col-1_row-2_button">
+                      <div className="Promo_box_download_item_col-1_row-2_button_icon-1"></div>
                     </div>
                   </a>
                 )}
-                {(large || !isIphone) && (
-                  <a href="https://play.google.com/store/apps/details?id=com.bitafit.client">
-                    <div className="Promo_box_download_item_col-1_row-2_button-1">
-                      <div className="Promo_box_download_item_col-1_row-2_button-1_icon-1"></div>
+                {(large || isIphone) && (
+                  <a href="https://apps.apple.com/ru/app/bitafit/id1591904801?l=en">
+                    <div className="Promo_box_download_item_col-1_row-2_button">
+                      <div className="Promo_box_download_item_col-1_row-2_button_icon-2"></div>
                     </div>
                   </a>
                 )}
